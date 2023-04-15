582ab3972e162f52ae9abd4bb3ed8e150960d69a
nigga <demoniaksins.com>
2023-04-15 16:25:21 +0200

    Third Commit

---
+++ navbar.js
@@ -0,0 +1,41 @@
+let token = JSON.parse(localStorage.getItem("token"));
+let name = JSON.parse(localStorage.getItem("userName"));
+
+let change = "LOG IN";
+
+if (token) {
+  change = "Hi " + name;
+}
+
+function navbar for (let index = 0; index < array.length; index++) 
+  const element = array[index];
+  for
+for (const key in object) {
+  if (Object.hasOwnProperty.call(object, key)) {
+    const element = object[key];
+    } function ( iterator, of, object) {
+  }
+}
+   {
+}
+  return <><div id="logo">
+    <a href="./index.html">
+      <img src="https://logodownload.org/wp-content/uploads/2014/05/zara-logo-1.png" alt="zaralogo">
+      </></a>
+  </div><div id="ship">
+      <div class="active">SHIPPING</div>
+      <div> {">"} </div>
+      <div>PAYMENT</div>
+      <div> &gt; </div>
+      <div>SUMMARY</div>
+    </div><div id="help">
+      <a href="./login.html">
+        <div>${change}</div>
+        <a />
+        <a href="./zarahelp.html">
+          <div>HELP</div>
+          <a />
+        </div>`;
+        &rbrace;
+
+        export default navbar;</></></>
